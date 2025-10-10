0170c6fde575ab7bda56ea2030466fba8369c6fc
chikwongyip <527439024@qq.com>
2025-10-10 23:09:38 +0800

    search

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,17 @@
-export default function page() {
-  return <p>Invoices Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function Page() {
+  return (
+    <div className='w-full'>
+      <div className='flex w-full items-center justify-between'>
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+    </div>
+  );
 }
