73923a28cee40ec693a85769a4f3a8522b986ac6
chikwongyip <527439024@qq.com>
2025-11-16 15:12:03 +0800

    auth

--- auth.config.ts
+++ auth.config.ts
@@ -11,7 +11,7 @@ export const authConfig = {
       if (isOnDashboard) {
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboar', nextUrl));
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     }
